e497f7fa7c43f4ddcf5a02606cd75c3117152ce6
cdey1 <300920267@cencol.ca>
2018-08-15 10:48:19 -0400

    first commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+(function(){
+
+    function Start() {
+        console.log(`%c App Started...`,"color:blue; font-size: 30px; font-weight:bold;");
+       // Your Code goes here
+
+    }
+
+    window.addEventListener("load", Start);
+})();
